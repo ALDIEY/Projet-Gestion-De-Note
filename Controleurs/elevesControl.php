5431438d327188b1715bf25a13b748f5f8ec4431
ALDIEY <cheikhaldieys@gmail.com>
2023-05-19 02:14:39 +0000

    second commit

---
+++ Controleurs/elevesControl.php
@@ -0,0 +1,24 @@
+
+<?php
+/*
+require ("../models/elevesModel.php");
+class ElevesController
+{   
+    private $model;
+    public function __construct()
+    {
+        $this->model = new Eleves();
+    }
+    
+    public function selectAll()
+    {
+        $recupDonnee = $this->model-> all();
+        echo json_encode($recupDonnee);
+    }
+    public function insertArticle()
+    {
+        $this->model->insert(['id' => 1, 'nom' => 'BA','prenom' => 'ADA','telephone' => '775435676',
+        'identifiant'=>'375', 'note' => 80]);
+    }
+}
+*/
---
+++ Models/Dbconnexion.php
@@ -0,0 +1,15 @@
+<?php
+/*
+class Dbconnexion
+{
+    public static function connexion()
+    {
+        return new PDO("mysql:host=localhost;dbname=GESTION_DE_NOTE","root","Alkir@320");
+    }
+
+    public function recupfetchAll($statement)
+    {
+        return $statement->fetchAll(PDO::FETCH_CLASS);
+    }
+}
+*/
